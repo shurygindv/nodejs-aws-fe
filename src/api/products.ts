d6ea60d325a78c507d7f7b7acd578eed6a7f304b
Daniil_Shurygin <shurygindv@ya.ru>
2020-11-08 23:55:16 +0400

    feat: endpoints for product creating (future)

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,11 +1,18 @@
 import API_PATHS from "constants/apiPaths";
+import { Product } from "models/Product";
 
 import axios from "axios";
 
 type Response = any;
 
-export const fetchProducts = () => {
-  return axios.get(API_PATHS.products).then((res: Response) => {
-    return res.data.result.items;
-  });
+const fetchProducts = () => {
+  return axios
+    .get(API_PATHS.products)
+    .then((res: Response) => res.data.result.items);
 };
+
+const addProductToBasketAsync = (product: Product) => {
+  return axios.post(API_PATHS.products, product);
+};
+
+export { addProductToBasketAsync, fetchProducts };
